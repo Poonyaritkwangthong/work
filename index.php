0317d84e9ec389709ccfa08dbb045cac1735fcea
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2022-12-02 09:38:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+  $name = "Poonyarit Kwangthong";
+  $weight ="66";
+  $hight ="170";
+  $age ="20";
+  $nationality ="thai";
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<div class="py-4 grid gap-x-8 gap-y-4 grid-cols-3">
+   <h1 class="select-none ... text-8xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ... text-white">Welcome to Website</h1>
+</div>
+<div class="text-center grid grid-rows-3 grid-flow-col gap-4 bg-black">
+   <h2 class="row-span-3 ... bg-white"></h2>
+   <h2 class="row-span-2 col-span-2 ... text-5xl text-white">Prototype Project</h2>
+   <h2 class="row-span-3 col-span-2 ...  bg-white"></h2>
+</div>
+<table class="border-collapse border border-slate-400 ... border-black">
+   <thead>
+    <tr>
+      <th class="border border-slate-300 ... text-3xl text-black">Menu</th>
+   </tr>  
+   </thead>
+   <tbody>
+      <tr>
+         <td class="border border-slate-300 ...">
+            ............
+         </td>
+      </tr>
+      <tr>
+         <td class="border border-slate-300 ...">
+            ............
+         </td>
+      </tr>
+      <tr>
+         <td class="border border-slate-300 ...">
+            ............
+         </td>
+      </tr>
+      <tr>
+         <td class="border border-slate-300 ...">
+            ............
+         </td>
+      </tr>
+      <tr>
+         <td class="border border-slate-300 ...">
+            .............
+         </td>
+      </tr>
+      <tr>
+         <td class="border border-slate-300 ...">
+            .............
+         </td>
+      </tr>
+    </tbody>
+</table>
+</body>
+</html>
